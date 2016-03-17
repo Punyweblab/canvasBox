af9ca1b13547d0704e6af57165b4bc4777874f20
Jefry <jefrycruz88@gmail.com>
2016-03-16 23:12:22 -0400

    Updated Models

--- app/User.php
+++ app/User.php
@@ -6,21 +6,19 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * User to project relationship
+     */
+    public function project(){
+        return $this->hasMany('App\Project');
+    }
 }
